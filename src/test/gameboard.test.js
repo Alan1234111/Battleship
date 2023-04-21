ac401981353c78488d48f6953b6fb3a15df96224
--unset <--unset>
2023-04-21 08:35:00 +0200

    Test which keep track of missed shots

--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -1,16 +1,19 @@
 const ship = require("../factories/Ship");
-const gameboard = require("../factories/Gameboard");
+const GameboardFactory = require("../factories/Gameboard");
+
+const gameboard = GameboardFactory.gameboard;
+const recordedShots = GameboardFactory.recordedShots;
 
 it("Check that hit area hit ship", () => {
   const destroyer = ship(2, [2, 3], [3, 3]);
-  gameboard(destroyer);
+
   gameboard(destroyer).receiveAttack(2, 3);
   expect(destroyer.getObject().numOfHit).toBe(1);
 
   gameboard(destroyer).receiveAttack(5, 4);
-  expect(destroyer.getObject().numOfHit).toBe(1);
+  expect(recordedShots).toEqual([[5, 4]]);
 
-  expect(gameboard(destroyer).getObject()).toBe("1");
+  // expect(gameboard(destroyer).getObject()).toBe("1");
 
   // expect().toEqual([5, 4]);
 });
