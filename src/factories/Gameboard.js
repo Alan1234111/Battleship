c434dfd2515add49d38f0bc57be43bcbf20ff301
--unset <--unset>
2023-04-21 09:32:51 +0200

    Save progress

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,15 +1,21 @@
+let recordedShots = [];
+
 const Gameboard = (ship) => {
   return {
     receiveAttack: (xHitCord, yHitCord) => {
       for (let i = 0; i < ship.getObject().xCords.length; i++) {
-        if (ship.getObject().xCords[i] == xHitCord && ship.getObject().yCords[i] == yHitCord) {
+        if (
+          ship.getObject().xCords[i] == xHitCord &&
+          ship.getObject().yCords[i] == yHitCord
+        ) {
           return ship.hit();
         }
       }
       recordedShots.push([xHitCord, yHitCord]);
     },
-    getObject: () => Gameboard,
+
+    isAllShipSunk: () => {},
   };
 };
 
-module.exports = Gameboard;
+module.exports = { gameboard: Gameboard, recordedShots: recordedShots };
--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -9,11 +9,27 @@ it("Check that hit area hit ship", () => {
 
   gameboard(destroyer).receiveAttack(2, 3);
   expect(destroyer.getObject().numOfHit).toBe(1);
+});
+
+it("Check that missed shot is recorder", () => {
+  const destroyer = ship(2, [2, 3], [3, 3]);
 
   gameboard(destroyer).receiveAttack(5, 4);
   expect(recordedShots).toEqual([[5, 4]]);
+});
 
-  // expect(gameboard(destroyer).getObject()).toBe("1");
+it("Check is all ships sunk", () => {
+  const ships = {
+    carrier: ship(5, [2, 3, 4, 5, 6], [1, 1, 1, 1, 1]),
+    battleship: ship(4, [2, 2, 2, 2], [1, 2, 3, 4]),
+    cruiser: ship(3, [4, 5, 6], [4, 4, 4]),
+  };
 
-  // expect().toEqual([5, 4]);
+  ships.forEach((ship) => {
+    for (let i = 0; i < ship.length; i++) {
+      ship.hit();
+    }
+  });
+
+  ships.forEach((ship) => {});
 });
