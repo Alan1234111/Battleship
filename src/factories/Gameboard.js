b203f793c22298b19445403733ad8b0f2d527f71
Alan Pason <alan.pason132@gmail.com>
2023-04-22 19:21:48 +0200

    Add: Gameboard factory chec is all Ship already Sunk

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,21 +1,22 @@
 let recordedShots = [];
 
-const Gameboard = (ship) => {
+const Gameboard = () => {
   return {
-    receiveAttack: (xHitCord, yHitCord) => {
+    receiveAttack: (ship, xHitCord, yHitCord) => {
       for (let i = 0; i < ship.getObject().xCords.length; i++) {
-        if (
-          ship.getObject().xCords[i] == xHitCord &&
-          ship.getObject().yCords[i] == yHitCord
-        ) {
+        if (ship.getObject().xCords[i] == xHitCord && ship.getObject().yCords[i] == yHitCord) {
           return ship.hit();
         }
       }
       recordedShots.push([xHitCord, yHitCord]);
     },
 
-    isAllShipSunk: () => {},
+    isAllShipSunk: (ships) => {
+      return ships.every((ship) => {
+        return ship.getObject().isSunk;
+      });
+    },
   };
 };
 
-module.exports = { gameboard: Gameboard, recordedShots: recordedShots };
+module.exports = {gameboard: Gameboard, recordedShots: recordedShots};
