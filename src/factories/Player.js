40ab97b4679e45c0ff8e8025691163be9c47f356
Alan Pason <alan.pason132@gmail.com>
2023-04-23 20:35:22 +0200

    Add: AiTurn and PlayerTurn

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -1,1 +1,30 @@
-const Player = () => {};
+const Player = () => {
+  const player = {
+    makeRandomMoves: () => {
+      return Math.floor(Math.random() * 7);
+    },
+
+    playerTurn: (gameboard, ships, xCord, yCord) => {
+      gameboard.receiveAttack(ships, xCord, yCord);
+    },
+
+    AiTurn: (gameboard, ships) => {
+      let xCord = player.makeRandomMoves();
+      let yCord = player.makeRandomMoves();
+      let isTheSame = gameboard.recordedShots.some((shot) => {
+        if (xCord === shot[0] && yCord === shot[1]) {
+          return true;
+        }
+      });
+
+      if (!isTheSame) {
+        return gameboard.receiveAttack(ships, xCord, yCord);
+      } else {
+        return player.AiTurn(gameboard, ships);
+      }
+    },
+  };
+  return player;
+};
+
+module.exports = Player;
