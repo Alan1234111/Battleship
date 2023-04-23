f9a6f78ab1d0c74973f4215c5371b1b3f660603d
Alan Pason <alan.pason132@gmail.com>
2023-04-23 17:43:47 +0200

    Change: factory to return object and fix reciveAttack function (the record shots does not saving)

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,22 +1,35 @@
-let recordedShots = [];
-
 const Gameboard = () => {
-  return {
-    receiveAttack: (ship, xHitCord, yHitCord) => {
-      for (let i = 0; i < ship.getObject().xCords.length; i++) {
-        if (ship.getObject().xCords[i] == xHitCord && ship.getObject().yCords[i] == yHitCord) {
-          return ship.hit();
+  const gameboard = {
+    recordedShots: [],
+
+    receiveAttack: function (ships, xHitCord, yHitCord) {
+      let hit = false;
+      ships.forEach((ship) => {
+        for (let i = 0; i < ship.xCords.length; i++) {
+          if (ship.xCords[i] == xHitCord && ship.yCords[i] == yHitCord) {
+            ship.hit();
+            hit = true;
+            break;
+          }
         }
+      });
+      if (!hit) {
+        this.recordShot([xHitCord, yHitCord]);
       }
-      recordedShots.push([xHitCord, yHitCord]);
+    },
+
+    recordShot: function (recordShot) {
+      this.recordedShots.push(recordShot);
     },
 
     isAllShipSunk: (ships) => {
       return ships.every((ship) => {
-        return ship.getObject().isSunk;
+        return ship.Sunk;
       });
     },
   };
+
+  return gameboard;
 };
 
-module.exports = {gameboard: Gameboard, recordedShots: recordedShots};
+module.exports = Gameboard;
