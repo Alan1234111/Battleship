261177777f1e6f0ba037d9de1bcff27d8d599580
Alan Pasoń <alan.pason132@gmail.com>
2023-04-26 19:53:20 +0200

    Add: PlaceShips, CreateShip, Gameboard contains ships change callback for this

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,13 +1,30 @@
+import Ship from "./Ship";
+
 const Gameboard = () => {
   const gameboard = {
     recordedShots: [],
+    ships: [],
 
-    receiveAttack: function (ships, xHitCord, yHitCord) {
-      let hit = false;
-      ships.forEach((ship) => {
+    createShip: function (lengthToSunk, xCords, yCords) {
+      return this.ships.push(Ship(lengthToSunk, xCords, yCords));
+    },
+
+    placeShips: function () {
+      this.ships.forEach((ship) => {
         for (let i = 0; i < ship.xCords.length; i++) {
-          if (ship.xCords[i] == xHitCord && ship.yCords[i] == yHitCord) {
+          const div = document.querySelector(`#player-board [data-x="${ship.xCords[i]}"][data-y="${ship.yCords[i]}"]`);
+          div.classList.add("ship");
+        }
+      });
+    },
+
+    receiveAttack: function (xHitCord, yHitCord) {
+      let hit = false;
+      this.ships.forEach((ship) => {
+        for (let i = 0; i < ship.xCords.length; i++) {
+          if (ship.xCords[i] === parseInt(xHitCord) && ship.yCords[i] === parseInt(yHitCord)) {
             ship.hit();
+            ship.isSunk();
             hit = true;
             break;
           }
@@ -22,8 +39,8 @@ const Gameboard = () => {
       this.recordedShots.push(recordShot);
     },
 
-    isAllShipSunk: (ships) => {
-      return ships.every((ship) => {
+    isAllShipSunk: function () {
+      return this.ships.every((ship) => {
         return ship.Sunk;
       });
     },
@@ -32,4 +49,4 @@ const Gameboard = () => {
   return gameboard;
 };
 
-module.exports = Gameboard;
+export default Gameboard;
