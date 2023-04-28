a4098d050379ef8598e16ab39db82fb81880ea1d
Alan Pasoń <alan.pason132@gmail.com>
2023-04-28 07:27:08 +0200

    Add: moves

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import "./style.css";
-// import Player from "./factories/Player";
+import Player from "./factories/Player";
 import Gameboard from "./factories/Gameboard";
 // import Ship from "./factories/Ship";
 
-function attack(gameboard, div) {
-  gameboard.receiveAttack(div.dataset.x, div.dataset.y);
-  if (gameboard.isAllShipSunk()) {
-    console.log("sds");
-  }
+const player = Player();
+const playerGameboard = Gameboard();
+
+const AiPlayer = Player();
+const AiGameboard = Gameboard();
+
+function makePlayerMove() {
+  const xCord = this.dataset.x;
+  const yCord = this.dataset.y;
+  player.playerTurn(playerGameboard, xCord, yCord);
+
+  setTimeout(() => {
+    makeAiMove();
+  }, 1000);
 }
 
-// const player = Player();
-const playerGameboard = Gameboard();
+function makeAiMove() {
+  AiPlayer.AiTurn(AiGameboard);
+}
+
 playerGameboard.createShip(5, [0, 1, 2, 3, 4], [0, 0, 0, 0, 0]);
 playerGameboard.createShip(4, [2, 2, 2, 2], [2, 3, 4, 5]);
 playerGameboard.createShip(3, [4, 5, 6], [6, 6, 6]);
@@ -19,25 +30,16 @@ playerGameboard.createShip(3, [7, 8, 9], [8, 8, 8]);
 playerGameboard.createShip(2, [9, 9], [4, 5]);
 playerGameboard.placeShips();
 
-const playerGameboardDivs = document.querySelectorAll("#player-board div");
-playerGameboardDivs.forEach((div) => {
-  div.addEventListener("click", () => {
-    attack(playerGameboard, div);
-  });
-});
+// const playerGameboardDivs = document.querySelectorAll("#player-board div");
 
-// const Ai = Player();
-const AiGameboard = Gameboard();
 AiGameboard.createShip(5, [0, 1, 2, 3, 4], [0, 0, 0, 0, 0]);
 AiGameboard.createShip(4, [2, 2, 2, 2], [2, 3, 4, 5]);
 AiGameboard.createShip(3, [4, 5, 6], [6, 6, 6]);
 AiGameboard.createShip(3, [7, 8, 9], [8, 8, 8]);
 AiGameboard.createShip(2, [9, 9], [4, 5]);
-AiGameboard.placeShips();
+// AiGameboard.placeShips('enemy-board');
 
 const AiGameboardDivs = document.querySelectorAll("#enemy-board div");
 AiGameboardDivs.forEach((div) => {
-  div.addEventListener("click", () => {
-    attack(AiGameboard, div);
-  });
+  div.addEventListener("click", makePlayerMove);
 });
